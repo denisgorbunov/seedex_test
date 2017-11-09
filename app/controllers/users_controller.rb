3cf3378cef3475ed8905bb8c0041a7e660169b99
Denis Gorbunov <denis@nodomain.me>
2017-11-09 15:12:39 +0300

    Обработка ошибок

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,21 @@
 class UsersController < ApplicationController
   def create
+    errors = []
     data = JSON.parse(request.body.read)
-    user = User.new(nickname: data['nickname'], last_online_at: Time.now)
-    if user.save
-      render json: {
-          success: true
-      }, status: 200
+    errors << "NICKNAME_EMPTY" unless data['nickname']
+    errors << "NICKNAME_TAKEN" if User.find_by(nickname: data['nickname'])
+    if errors.empty?
+      user = User.new(nickname: data['nickname'], last_online_at: Time.now)
+      if user.save
+        render json: {
+            success: true
+        }, status: 200
+      end
     else
       render json: {
           success: false,
-          error: @user.errors
+          error: errors
       }, status: 400
     end
   end
 end
-
-#TODO: Обработка ошибок
