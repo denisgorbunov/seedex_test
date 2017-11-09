336172cc5917d182975c98905b542f9a21571198
Denis Gorbunov <denis@nodomain.me>
2017-11-09 19:05:15 +0300

    Связал модели

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,13 @@
 class Message < ApplicationRecord
+  belongs_to :receiver, class_name: "User", foreign_key: "receiver_id", inverse_of: :income
+  belongs_to :sender, class_name: "User", foreign_key: "sender_id", inverse_of: :outcome
+
+  validates :message, :sender_id, :receiver_id, presence: true
+
   scope :dialog, ->(sender, receiver) do
     where('(sender_id= ? AND receiver_id= ?) OR (sender_id= ? AND receiver_id= ?)',
           sender, receiver, receiver, sender).order(:id)
   end
+
+  scope :unread, -> { where(read: false) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+  has_many :income, class_name: "Message", foreign_key: "receiver_id", inverse_of: :receiver
+  has_many :outcome, class_name: "Message", foreign_key: "sender_id", inverse_of: :sender
+
   validates :nickname, presence: true, uniqueness: true
+
 end
