4418f6feb423aef15841fb26136d570dd7edeadb
Denis Gorbunov <denis@nodomain.me>
2017-11-09 19:05:38 +0300

    промежуточный коммит

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,37 +2,36 @@ class UsersController < ApplicationController
   def create
     errors = []
     data = JSON.parse(request.body.read)
-    errors << "NICKNAME_EMPTY" unless data['nickname']
-    errors << "NICKNAME_TAKEN" if User.find_by(nickname: data['nickname'])
+    errors << 'NICKNAME_EMPTY' unless data['nickname']
+    errors << 'NICKNAME_TAKEN' if User.find_by(nickname: data['nickname'])
     if errors.empty?
       user = User.new(nickname: data['nickname'], last_online_at: Time.now)
       if user.save
         render json: {
-            success: true
+          success: true
         }, status: 200
       end
     else
       render json: {
-          success: false,
-          error: errors
+        success: false,
+        error: errors
       }, status: 400
     end
   end
 
   def index
     users = []
-    User.all.map do |m|
-      attr = m.attributes.to_h
-      attr[:unread_messages] = getUnread(m.id).size
-      users << attr
+    User.all.map do |u|
+      attr = u.attributes.to_h
+      attr[:unread_messages] = u.income.unread.size
+      users << attr if attr[:unread_messages] > 0
+      users.sort_by! { |a| a[:last_online_at] }.reverse!
     end
-    render json: {
-        success: true,
-        users: users
-    }, status: 200
-  end
+    #users.sort_by! { |a| a[:last_online_at] }.reverse!
 
-  def getUnread(user)
-    Message.where(receiver_id: user, read: false)
+    render json: {
+      success: true,
+      users: users
+    }, status: 200
   end
 end
